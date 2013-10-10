424a5cffa21243af1a563c74a3eab146260d396d
Xavier Sala <utrescu@gmail.com>
2013-10-11 00:28:38 +0200

    S'afegeixen múltiples comentaris i JavaDoc per fer el text més entenedor

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 guerradebarcos
 ==============
 
-Projecte de creació del joc de guerra de barcos en objectes Java. Fet a classe com a exemple
+Projecte de creació del joc de guerra de barcos en objectes Java. La idea és 
+crear un joc que permeti a dos jugadors jugar al joc de la guerra de barcos
+
+Els taulers seran de 9x9 referenciats per números i lletres tal com es pot 
+veure en l'esquema. I els barcos es podran posar horitzontalment com 
+verticalment. Hi poden haver barcos de diferents mides:
+
+   |0|1|2|3|4|5|6|7|8|9|
+  ----------------------
+  A| | | |%|%|%| | | | |
+  ----------------------
+  B| | | | | | | | | | |
+  ----------------------
+  C| |%| | | | | | | | |
+  ----------------------
+  D| |%| | | | | | | | | 
+  ----------------------
+  E| | | | | | | | | | | 
+  ----------------------
+  F| | | | | |%|%|%|%|%| 
+  ----------------------
+  G| | | | | | | | | | | 
+  ----------------------
+  H| | |%| | |%| | | | | 
+  ----------------------
+  I| | |%| | | | | | | | 
+  ----------------------
+  
+Fet a classe de Programació com a exemple de Programació Orientada a Objectes en
+llenguatge Java
+
--- src/net/xaviersala/barcos/Barco.java
+++ src/net/xaviersala/barcos/Barco.java
@@ -2,9 +2,25 @@ package net.xaviersala.barcos;
 
 import java.util.ArrayList;
 
+/** 
+ * La classe Barco representa un barco en el joc de
+ * la guerra de barcos.
+ * 
+ * Donat que la seva posició serà dins del tauler es
+ * guarda la posició que tindrà cada una de les peces
+ * del barco en el tauler.
+ * 
+ * @author Xavier
+ *
+ */
 public class Barco {
     ArrayList<String> posicions;
     
+    /**
+     * Constructor de barcos a partir d'una llista de posicions.
+     * 
+     * @param posicions Rep les posicions del barco com a paràmetres
+     */
     public Barco(String[] posicions) {
         this.posicions = new ArrayList<String>();
         for(String posicio: posicions) {
@@ -12,6 +28,18 @@ public class Barco {
         }
     }
 
+    /**
+     * Funció que comprova si han tocat el barco a partir d'una posició
+     * que li especifiquem.
+     * 
+     * Les posicions estaran en format text "A1", "E6", etc..
+     * 
+     * En cas de que la posició hi sigui s'elimina del barco perquè 
+     * aquesta posició ha estat "enfonsada"
+     * 
+     * @param lloc La posició a comprovar
+     * @return Retorna si el barco ha estat tocat o no
+     */
     public boolean comprovaPosicio(String lloc) {
         
         int pesa = posicions.indexOf(lloc);
